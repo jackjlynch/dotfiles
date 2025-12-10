17470a28aa55d713c76759378d569d9e0a303b5d
Jack Lynch <jack.lynch@bytedance.com>
2025-12-10 13:22:31 -0800

    fix startup error

--- vim/.vim/lua/config.lua
+++ vim/.vim/lua/config.lua
@@ -1,13 +1,14 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
-
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require("mason-lspconfig").setup_handlers {
-  function (server_name)
-    require("lspconfig")[server_name].setup {
-      capabilities = capabilities
-    }
-  end
+require("mason-lspconfig").setup {
+  ensure_installed = { "lua_ls", "ts_ls", "eslint", "rust_analyzer" },
+  handlers = {
+    function (server_name)
+      require("lspconfig")[server_name].setup {
+        capabilities = capabilities
+      }
+    end
+  }
 }
 
 -- Set up nvim-cmp.
